0f541ac999730ae4cd6c3df9ba6b704589606d39
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-08 02:08:22 -0600

    added functionality to my btnStartEl

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const btnStartEl = document.querySelector(".btn-start");
-const pTag = document.querySelector("p");
-
-let activeQuestion = 1;
-
 const questions = [
   {
-    question1: "What was the name of the first Indiana Jones movie?",
+    question: "What was the name of the first Indiana Jones movie?",
     choice1: "The Raiders of the Lost Ark",
     choice2: "The Temple of Doom",
     choice3: "The Last Crusade",
@@ -13,7 +8,7 @@ const questions = [
     answer: "The Raiders of the Lost Ark",
   },
   {
-    question1: "Who plays Indiana Jones?",
+    question: "Who plays Indiana Jones?",
     choice1: "Mel Gibson",
     choice2: "Leonardo Di Caprio",
     choice3: "Harrison Ford",
@@ -21,6 +16,21 @@ const questions = [
     answer: "Harrison Ford",
   },
 ];
+
+const btnStartEl = document.querySelector(".btn-start");
+const currentQuestionEl = document.querySelector(".current--question");
+const choice1El = document.querySelector("#choice1");
+const choice2El = document.querySelector("#choice2");
+const choice3El = document.querySelector("#choice3");
+const choice4El = document.querySelector("#choice4");
+const choiceBtn = document.querySelector(".choice--btn");
+
 btnStartEl.addEventListener("click", function () {
-  pTag.classList.add("hidden");
+  choiceBtn.classList.remove("hidden");
+  currentQuestionEl.innerHTML = questions[0].question;
+  choice1El.innerHTML = questions[0].choice1;
+  choice2El.innerHTML = questions[0].choice2;
+  choice3El.innerHTML = questions[0].choice3;
+  choice4El.innerHTML = questions[0].choice4;
+  btnStartEl.textContent = "Next";
 });
