f6ff2e3a8471b8308c046dfde36c71cf7d885975
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-09 10:49:07 -0600

    refactored code

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ const questions = [
     answer: "Henry Jones, Sr.",
   },
   {
-    question:
-      'When did "Indiana Jones and the Raiders of the Lost Ark" release?',
+    question: 'When did "Indiana Jones and the Raiders of the Lost Ark" release?',
     choice1: "1979",
     choice2: "1984",
     choice3: "1978",
@@ -41,7 +40,6 @@ const questions = [
     answer: "1981",
   },
 ];
-let activeQuestion = 0;
 const btnStartEl = document.querySelector(".btn-start");
 const btnNextEl = document.querySelector(".btn-next");
 const btnFinishEl = document.querySelector(".btn-finish");
@@ -54,6 +52,12 @@ const choice4El = document.querySelector("#choice4");
 const choiceBtn = document.querySelectorAll(".choice--btn");
 const highlightAnswerEl = document.querySelector(".highlight-answer");
 const startText = document.querySelector(".start-text");
+const scoreEl = document.querySelector(".score");
+const scoreHolderEl = document.querySelector(".score-holder");
+const bodyStyleEl = document.querySelector("body-style");
+
+let score = 0;
+let activeQuestion = 0;
 
 function updateValues() {
   currentQuestionEl.textContent = questions[activeQuestion].question;
@@ -70,7 +74,12 @@ function removeUnselectedOption(selectedOption) {
     }
   });
 }
-
+function removeHiddenAndHighlight() {
+  choice1El.classList.remove("hidden", "highlight-answer");
+  choice2El.classList.remove("hidden", "highlight-answer");
+  choice3El.classList.remove("hidden", "highlight-answer");
+  choice4El.classList.remove("hidden", "highlight-answer");
+}
 choiceBtn.forEach((item) => {
   item.addEventListener("click", function () {
     if (!item.classList.contains("highlight-answer")) {
@@ -84,28 +93,33 @@ choiceBtn.forEach((item) => {
 
 btnStartEl.addEventListener("click", function () {
   updateValues();
-  choice1El.classList.remove("hidden");
-  choice2El.classList.remove("hidden");
-  choice3El.classList.remove("hidden");
-  choice4El.classList.remove("hidden");
+  removeHiddenAndHighlight();
   currentQuestionEl.classList.remove("hidden");
-  btnStartEl.classList.add("hidden");
   btnNextEl.classList.remove("hidden");
+  scoreHolderEl.classList.remove("hidden");
   startText.classList.add("hidden");
+  btnStartEl.classList.add("hidden");
 });
 
 btnNextEl.addEventListener("click", function () {
-  if (questions[activeQuestion]) {
-    choice1El.classList.remove("highlight-answer");
-    choice2El.classList.remove("highlight-answer");
-    choice3El.classList.remove("highlight-answer");
-    choice4El.classList.remove("highlight-answer");
-    activeQuestion++;
-    updateValues();
-    console.log(activeQuestion);
-    if (activeQuestion === 4) {
-      btnNextEl.classList.add("hidden");
-      btnFinishEl.classList.remove("hidden");
-    }
+  removeHiddenAndHighlight();
+  activeQuestion++;
+  updateValues();
+  console.log(activeQuestion);
+  if (activeQuestion === 4) {
+    btnNextEl.classList.add("hidden");
+    btnFinishEl.classList.remove("hidden");
   }
+  choiceBtn.forEach((item) => {
+    if (choiceBtn.classList.contains("highlight-answer")) {
+      if (item.answer === answer[activeQuestion].answer) {
+        score++;
+        scoreHolderEl.textContent = `Score : ${score}/5`;
+      }
+    }
+  });
 });
+
+// btnFinishEl.addEventListener("click", function () {
+//   bodyStyleEl.classList.add("hidden");
+// });
